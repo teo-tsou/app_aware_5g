c4ff3da4718cfd856dd5ecb42519fc12cac8027c
teo-tsou <ttsourdinis@gmail.com>
2021-11-03 15:35:36 +0200

    Pushing Jupyter Notebook

--- parser/parser.py
+++ parser/parser.py
@@ -9,7 +9,7 @@ with open('traffic.csv', 'w', newline='') as traffic:
     writer.writerow(["Packet_Num", "Time", "Source", "Destination", "Protocol", "Length"])
     try:
         for packet in cap:
-            if (packet.protocol == 'UDP' or packet.protocol == 'SIP' or (packet.protocol == 'HTTP' and (int(packet.length) == 168 or int(packet.length) == 681 ))) and (ipaddress.ip_address(packet.destination) in ipaddress.ip_network('192.168.20.0/24') or ipaddress.ip_address(packet.source) in ipaddress.ip_network('192.168.20.0/24')):
+            if (packet.protocol == 'UDP' or packet.protocol == 'SIP' or (packet.protocol == 'HTTP' and (int(packet.length) == 168 or int(packet.length) == 681 ))):
                 formattedLine = str(packet).split(" ")[0:6]
                 wr = csv.writer(traffic, quoting=csv.QUOTE_ALL)
                 wr.writerow(formattedLine[0:6])  
