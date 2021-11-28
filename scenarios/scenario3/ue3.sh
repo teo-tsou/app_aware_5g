04e74de225de81ed209baa251c352100a5cdd3b4
Ilias <iliasece16@gmail.com>
2021-11-28 18:18:20 +0200

    new scenarios with half duration

---
+++ scenarios/scenario3/ue3.sh
@@ -0,0 +1,57 @@
+sleep 22
+
+#WEB-SERVER
+start_time=$(date +%s)
+# perform a task
+end_time=$(date +%s)
+
+#elapsed time with second resolution
+elapsed=$(( end_time - start_time ))
+while [ $elapsed -lt 8 ]
+do
+        curl -H 'Cache-Control: no-cache' 192.168.3.103:80
+        sleep 0.1
+        echo "Elapsed time: $elapsed"
+        end_time=$(date +%s)
+        elapsed=$(( end_time - start_time ))
+done
+
+sleep 17
+
+#WEB-SERVER
+start_time=$(date +%s)
+# perform a task
+end_time=$(date +%s)
+
+#elapsed time with second resolution
+elapsed=$(( end_time - start_time ))
+while [ $elapsed -lt 3 ]
+do
+        curl -H 'Cache-Control: no-cache' 192.168.3.103:80
+        sleep 0.1
+        echo "Elapsed time: $elapsed"
+        end_time=$(date +%s)
+        elapsed=$(( end_time - start_time ))
+done
+
+#WEB-RTC
+sudo chromium-browser 192.168.3.101:8000 --no-sandbox &
+sleep 52
+pkill chromium
+
+
+#WEB-SERVER
+start_time=$(date +%s)
+# perform a task
+end_time=$(date +%s)
+
+#elapsed time with second resolution
+elapsed=$(( end_time - start_time ))
+while [ $elapsed -lt 50 ]
+do
+        curl -H 'Cache-Control: no-cache' 192.168.3.103:80
+        sleep 0.1
+        echo "Elapsed time: $elapsed"
+        end_time=$(date +%s)
+        elapsed=$(( end_time - start_time ))
+done
