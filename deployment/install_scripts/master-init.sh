30371155e0d71aed0d09a8e58781c1947e6bba70
Theodoros Tsourdinis <56459281+teo-tsou@users.noreply.github.com>
2022-01-07 23:37:48 +0200

    Update master-init.sh

--- deployment/install_scripts/master-init.sh
+++ deployment/install_scripts/master-init.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-sudo route delete default gw 10.0.1.200 eth0
-sudo route add default gw 10.64.92.1 eth1
 kubeadm init --pod-network-cidr=10.244.0.0/16 --control-plane-endpoint=master && mkdir -p $HOME/.kube && sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config && sudo chown $(id -u):$(id -g) $HOME/.kube/config && sudo kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml && sleep 10 && kubectl delete --all po -n kube-system
 sleep 5 && kubectl get po -A
 kubeadm token create --print-join-command
