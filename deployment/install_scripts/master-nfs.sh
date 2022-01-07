e72c76315116619484453786b21c0d51cd6ac21e
Theodoros Tsourdinis <56459281+teo-tsou@users.noreply.github.com>
2022-01-07 23:36:20 +0200

    Update master-nfs.sh

--- deployment/install_scripts/master-nfs.sh
+++ deployment/install_scripts/master-nfs.sh
@@ -1,3 +1,3 @@
-!/bin/bash
+#!/bin/bash
 sudo apt-get install -y nfs-kernel-server && sudo mkdir -p /mnt/nfs_server_files && echo '/mnt/nfs_server_files  *(rw,insecure,sync,no_subtree_check,no_root_squash)' >> /etc/exports && exportfs -a && exportfs -a
 
