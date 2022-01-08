63baa2ae8638f32183a1076a3d335b7dd5db1468
teo-tsou <ttsourdinis@gmail.com>
2022-01-08 22:44:13 +0200

    script updated

--- deployment/install_scripts/master-init.sh
+++ deployment/install_scripts/master-init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-kubeadm init --pod-network-cidr=10.244.0.0/16 --control-plane-endpoint=master && mkdir -p $HOME/.kube && sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config && sudo chown $(id -u):$(id -g) $HOME/.kube/config && sudo kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml && sleep 10
+kubeadm init --pod-network-cidr=10.244.0.0/16 --control-plane-endpoint=master && mkdir -p $HOME/.kube && sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config && sudo chown $(id -u):$(id -g) $HOME/.kube/config && sudo kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml && sleep 15
 sleep 5 && kubectl get po -A
 kubeadm token create --print-join-command
