e7ff2ecbd7c16ae2df0b07134050931bd4cd1633
Tsou <ttsourdinis@gmail.com>
2022-01-10 12:32:23 +0200

    deleting old scripts

--- parser/df_from_db.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-import pandas as pd
-
-db_connection_str = 'mysql://root:password@mysql.default/test'
-db_connection = create_engine(db_connection_str)
-
-df = pd.read_sql('SELECT * FROM test.messages', con=db_connection)
-print(df)
-db_connection.dispose() 
